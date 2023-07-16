c394d1cad8085d39e6ae237cf407597c970f7c12
ShayanSir <125733674+ShayanSir@users.noreply.github.com>
2023-07-15 23:36:46 -0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/DashBoard.dart';
+
+import 'Theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme,
+      darkTheme: MyTheme.darkTheme,
+      home: const Dashboard(),
+    );
+  }
+}
+
